ecaa88e538cd79937fdde1000722650433d0a9aa
ilo80 <ilo80.pro@gmail.com>
2024-12-22 00:10:16 +0100

    fix: fix parsing in case of empty string

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -1,6 +1,10 @@
 import { Color } from "../models/utils";
 
 export function parseRGBColor(color: string): Color {
+    if (!color) {
+        return { r: 0, g: 0, b: 0 };
+    }
+
     const [r, g, b] = color.split(',').map(Number);
     return { r, g, b };
 }
--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,9 +1,17 @@
-export function parseDateFromDDMMYYYY(date: string): Date {
-    const [day, month, year] = date.split("/").map(Number);
+export function parseDateFromDDMMYYYY(dateString: string): Date {
+    if (!dateString) {
+        return new Date();
+    }
+
+    const [day, month, year] = dateString.split("/").map(Number);
     return new Date(year, month - 1, day);
 }
 
 export function parseDateFromDDMMYYYYHHMM(dateString: string): Date {
+    if (!dateString) {
+        return new Date();
+    }
+
     const [datePart, timePart] = dateString.split(' ');
     const [day, month, year] = datePart.split('/').map(num => parseInt(num, 10));
     const [hours, minutes] = timePart.split(':').map(num => parseInt(num, 10));
